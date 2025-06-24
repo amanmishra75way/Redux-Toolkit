57b425adc0b711458734fb98de4834ee202ae0fe
amanmishra75way <amishra.75way@gmail.com>
2025-06-24 17:59:20 +0530

    feat: integrate Tailwind CSS and create user list component
    - Added Tailwind CSS and PostCSS configuration to the project.
    - Created a new Component.jsx to fetch and display a list of users.
    - Updated main.jsx to render the new Component instead of App.
    - Added Tailwind CSS directives in index.css for styling.
    - Updated package.json to include necessary dependencies for Tailwind CSS and PostCSS.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import Component from "./Component.jsx";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <Component />
   </Provider>
 );
