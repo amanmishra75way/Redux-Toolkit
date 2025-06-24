0e4bf4f26fdb0d275b36424e958230d48d80999d
amanmishra75way <amishra.75way@gmail.com>
2025-06-24 17:06:13 +0530

    feat: add input for incrementing counter by specified amount

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { incrementByAmount, increment, decrement } from "./faetures/counterSlice";
 const App = () => {
   const dispatch = useDispatch();
 
   const count = useSelector((state) => state.counter.value);
+  const [amount, setAmount] = useState(false);
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", maxWidth: "20vw", gap: "20px" }}>
       <button onClick={() => dispatch(increment())}>+</button>
       Count is: {count}
       <button onClick={() => dispatch(decrement())}>-</button>
+      <div>
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" />
+        <button onClick={() => dispatch(incrementByAmount(Number(amount)))}>Add By amount</button>
+      </div>
     </div>
   );
 };
